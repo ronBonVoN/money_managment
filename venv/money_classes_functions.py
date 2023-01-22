24ef1f6890948a198c293d7219bfd75038801e06
ronBonVoN <veronika.l.davis@gmail.com>
2023-01-21 23:03:56 -0500

    trying to fix bugs

--- venv/money_classes_functions.py
+++ venv/money_classes_functions.py
@@ -15,29 +15,30 @@ class Transaction():
     def view(self):
         return f'{self.date}: ${self.amt} {self.type}, reason: {self.reason}'
 
-class Log:
+class Log():
     def __init__(self, start, end):
+        super().__init__()
         self.start = start
         self.end = end
         self.txt = self.create_txt() #create text file
         self.dict = self.build_dict() #dictionary to record all transactions
 
     def create_txt(self):
-        self.txt = open(f'money log: {self.start}-{self.end}.txt', 'a')
+        self.txt = open(f'money log {self.start}-{self.end}.txt', 'a')
         self.txt.close()
 
     def build_dict(self):
         self.dict = {}
         date = datetime.strptime(self.start, '%m.%d.%y')
         while date != datetime.strptime(self.end, '%m.%d.%y'):
-            self.dict[date.strftime('%m.%d.%y')]  = []
+            self.dict[str(date)] = []
             date += timedelta(days=1)
 
     def add(self, date, transaction):
-        self.dict[date] = self.dict[date].append(transaction)
+        self.dict[datetime.strptime(date, '%m.%d.%y')] = self.dict[date].append(transaction)
 
     def write(self, str):
-        file = open(self.pathname, 'w')
+        file = open(self.txt, 'w')
         file.write(str)
         file.close()
 
@@ -45,6 +46,7 @@ def withdraw(log, amt, date, reason):
     withdraw = Transaction(amt, 'withdraw', date, reason)
     log.add(date, withdraw)
     log.write(withdraw.view())
+    print('done')
 
 def deposite(log, amt, date, reason):
     deposite = Transaction(amt, 'deposite', date, reason)
--- venv/ui.py
+++ venv/ui.py
@@ -3,7 +3,7 @@ from tkinter import *
 import money_classes_functions as M
 import current_log as CL
 
-font = ('calibri', 12)
+font = ('calibri', 11)
 
 win = tk.Tk()
 win.title('money_managment')
@@ -13,12 +13,12 @@ gridx = 40
 gridy = 20
 
 drop_options = ['withdraw', 'deposite']
-type = StringVar()
+type = StringVar(win)
 type.set(drop_options[0])
 drop = tk.OptionMenu(win, type, *drop_options)
 drop.grid(row=0, column=0, padx=gridx, pady=gridy)
 
-amt_label = tk.Label(win, text='amt')
+amt_label = tk.Label(win, text='amt', font=font)
 amt_label.grid(row=0, column=1)
 
 amt_input = tk.Text(win, font=font, width=10, height = 1)
@@ -27,18 +27,23 @@ amt_input.grid(row=0, column=2, padx=gridx, pady=gridy)
 reason_label = tk.Label(win, text='reason', font=font)
 reason_label.grid(row=0, column=3, padx=gridx, pady=gridy)
 
-reason_imput = tk.Text(win, font=font, width=20, height=1)
-reason_imput.grid(row=0, column=4, padx=gridx, pady=gridy)
+reason_input = tk.Text(win, font=font, width=20, height=1)
+reason_input.grid(row=0, column=4, padx=gridx, pady=gridy)
+
+date_label = tk.Label(win, text='date', font=font)
+date_label.grid(row=1, column=0, padx=gridx, pady=gridy)
+
+date_input = tk.Text(win, font=font, width=20, height=1)
+date_input.grid(row=1, column=1, padx=gridx, pady=gridy)
 
 
 def enter_command():
-    if type=='withdraw':
-        pass
-       # M.withdraw(CL.log, -int(amt_input.get()),
+    if type.get()=='withdraw':
+       M.withdraw(CL.log, -int(amt_input.get(1.0, "end-1c")), date_input.get(1.0, "end-1c"), reason_input.get(1.0, "end-1c"))
 
 
-enter = tk.Button(win, text='enter', width=10, anchor='center')
-enter.grid(row=1, column=0, columnspan=4)
+enter = tk.Button(win, text='enter', width=10, anchor='center', command=enter_command)
+enter.grid(row=2, column=0, columnspan=4)
 
 win.mainloop()
 
